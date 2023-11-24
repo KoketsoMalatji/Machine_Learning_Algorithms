a7ec621959da00cc253a54900ab5d8509b9b9418
Koketso Malatji <koketsosean209@gmail.com>
2023-11-24 21:36:36 +0200

    Algorithm

--- 5-Fold-Cross-Validation.py
+++ 5-Fold-Cross-Validation.py
@@ -1,26 +1,18 @@
-# Import necessary libraries
-from sklearn.datasets import make_classification
-from sklearn.model_selection import cross_val_score, KFold, train_test_split
+from sklearn.model_selection import cross_val_score, KFold
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_breast_cancer
 
-# Generate a dummy dataset (replace this with your dataset loading/preparation code)
-X, y = make_classification(n_samples=1000, n_features=20, n_classes=2, random_state=42)
+# Load Breast Cancer dataset
+cancer = load_breast_cancer()
+X_cancer, y_cancer = cancer.data, cancer.target
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Initialize RandomForestClassifier (you can replace this with other classifiers)
+clf = RandomForestClassifier()
 
-# Choose your classification model
-model = RandomForestClassifier()  # Replace with your chosen classification model
-
-# Implement 5-fold cross-validation
-num_folds = 5
-kf = KFold(n_splits=num_folds, shuffle=True, random_state=42)
-accuracy_scores = cross_val_score(model, X_train, y_train, cv=kf, scoring='accuracy')
+# Perform 5-fold cross-validation
+kfold = KFold(n_splits=5, shuffle=True, random_state=42)
+scores = cross_val_score(clf, X_cancer, y_cancer, cv=kfold)
 
 # Compute average accuracy
-average_accuracy = sum(accuracy_scores) / num_folds
-
-# Print the average accuracy
-print(f"Average Accuracy: {average_accuracy:.4f}")
-
-
+average_accuracy = scores.mean()
+print("Average Accuracy:", average_accuracy)
